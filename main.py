423407e81a31a4155253434c45eeb488da6fb29d
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:38:41 +0900

    train_file_list_to_json implemented

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     template_mid = '\",\"German\":\"'
     template_end = '\"}'
 
+<<<<<<< HEAD
     # Can this be working?
     processed_file_list = []
     for english_file, german_file in zip(english_file_list, german_file_list):
@@ -33,6 +34,15 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
         processed_file_list.append(template_end + english_file + template_mid + german_file + template_start)
     return processed_file_list
 
+=======
+    processed_file_list = []
+    for english_file, german_file in zip(english_file_list, german_file_list):
+        english_file = process_file(english_file)
+        german_file = process_file(german_file)
+
+        processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
+    return processed_file_list
+>>>>>>> 3909f1b (train_file_list_to_json implemented)
 
 def write_file_list(file_list: List[str], path: str) -> None:
     """Writes a list of strings to a file, each string on a new line"""
